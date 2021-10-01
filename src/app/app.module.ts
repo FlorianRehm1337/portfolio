15efd979d580aa86d97145b5c7ca641b813c6266
FlorianRehm1337 <florehm1337@gmail.com>
2021-10-01 17:35:59 +0200

    greeting and skills finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { GreetingComponent } from './greeting/greeting.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    GreetingComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    AboutMeComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
