0c6b8b594e5b52449ab08f9edc0f7cec8a9e8ba9
FlorianRehm1337 <florehm1337@gmail.com>
2021-10-03 16:16:23 +0200

    worked on About me

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
-
+kochwelt_bg = '/assets/img/project-kochwelt.jpg'
   constructor() { }
 
   ngOnInit(): void {
